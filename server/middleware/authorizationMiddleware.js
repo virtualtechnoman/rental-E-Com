6244986ba10ff5d765b76d07e9840885ee613c8e
vaibhav Thangriyal <vaibhavthangriyal@gmail.comgit config --global user.name vaibhavthangriyal>
2019-08-12 07:44:36 +0430

    CUstomer done

--- server/middleware/authorizationMiddleware.js
+++ server/middleware/authorizationMiddleware.js
@@ -1,10 +1,8 @@
 module.exports = (privilege) => {
     return (req, res, next) => {
-        console.log(privilege);
-        console.log("REQ.User", req.user.role);
-        console.log("Privilege", privilege);
-        console.log("REQ>USER>Prevligies", req.user.role.privileges.ADD_NEW_USER);
+        console.log("PRI", privilege)
         if (req.user.role && req.user.role.privileges[privilege]) {
+            console.log(req.user.role.privileges.privilege);
             next();
         } else {
             res.status(403).json({ status: 403, data: null, error: true, message: "Forbidden" });
