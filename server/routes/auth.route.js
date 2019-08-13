22e4129870001e14528f8473e168b639c5fe4e49
bhumanyukumar <bhumanyukumar@gmail.com>
2019-08-13 15:13:30 +0530

    user api done

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -84,90 +84,90 @@ router.post('/register', async (req, res) => {
     })
   }
 });
-router.post('/login', async(req,res)=>{
+router.post('/login', async (req, res) => {
   // console.log("LOGIN ROUTE")
   if (req.headers.token != undefined) {
-      if (typeof req.headers.token == "string" && req.headers.token.trim() !== "") {
-          // console.log("HAS TOKEN");
-              jwt.verify(req.headers.token, process.env.JWT_SECRET, (err, payload) => {
-                  if (err) {
-                      console.log(err);
-                      res.status(400).json({status:400, errors:true, data:null, message: "Invalid token" })
-                  } else {
-                      User.findById(payload._id, (err, doc) => {
-                          if (err) {
-                            console.log(err);
-                              return res.status(500).json({ status: 500, errors: true, data: null, message: "Error while validating your token details" })
-                          }
-                          if (doc) {
-                              let u = doc.toObject();
-                              delete u.password;
-                              req.user = u;
-                              res.json({ status: 200, data: u, errors: false, message: "You are already logged in!" });
-                          } else {
-                              return res.status(500).json({ status:500, data:null, errors:true, message: "Error while validating your token details" })
-                          }
-                      })
-                      // next();
-                  }
-              })
-      } else {
-          res.json({ message: "invalid token" });
-      }
-  } else {
-      let result = UserController.verifyLogin(req.body);
-      // console.log("NO TOKEN");
-      if (!isEmpty(result.errors)) {
-          return res.json(result.errors);
-      }
-      if (req.body.email && req.body.password) {
-          let email = req.body.email.trim();
-          console.log("BODY IS ", req.body);
-          console.log("Email IS ", email);
-          User.findOne({ email: email }, (err, user) => {
-              // console.log(user)
-              if (err) {
-                console.log(err);
-                  return res.status(500).json({ status:500, data:null, errors:true, message: "Error while finding the user" });
-              }
-              if (user) {
-                  // console.log("USER FOUND")
-                  bcrypt.compare(req.body.password, user.password, function (er, isMatched) {
-                      if (er) {
-                          console.log(er);
-                          return res.status(401).json({ message: "Error in validating Credentials" });
-                      }
-                      if (isMatched) {
-                          const u = user.toObject();
-                          delete u.password;
-                          req.user = u;
-                          jwt.sign({ _id: u._id }, process.env.JWT_SECRET, function (err, token) {
-                              if (err) {
-                                  console.log(err);
-                                  res.json({ message: "Error while generating token" })
-                              }
-                              else {
-                                  user = user.toObject();
-                                  delete user.password;
-                                  req.user = user;
-                                  res.json({ message: "Login successfull", token, user })
-                              }
-                          });
-                      } else {
-                          return res.status(401).json({ message: "Invalid Credentials" });
-                      }
-                  });
-              } else {
-                  res.json({ message: "User not exist" });
-              }
-
+    if (typeof req.headers.token == "string" && req.headers.token.trim() !== "") {
+      // console.log("HAS TOKEN");
+      jwt.verify(req.headers.token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) {
+          console.log(err);
+          res.status(400).json({ status: 400, errors: true, data: null, message: "Invalid token" })
+        } else {
+          User.findById(payload._id, (err, doc) => {
+            if (err) {
+              console.log(err);
+              return res.status(500).json({ status: 500, errors: true, data: null, message: "Error while validating your token details" })
+            }
+            if (doc) {
+              let u = doc.toObject();
+              delete u.password;
+              req.user = u;
+              res.json({ status: 200, data: u, errors: false, message: "You are already logged in!" });
+            } else {
+              return res.status(500).json({ status: 500, data: null, errors: true, message: "Error while validating your token details" })
+            }
           })
-          // let password = req.body.password;
-          // User.find({ email: email, })
-      }
+          // next();
+        }
+      })
+    } else {
+      res.json({ message: "invalid token" });
+    }
+  } else {
+    let result = UserController.verifyLogin(req.body);
+    // console.log("NO TOKEN");
+    if (!isEmpty(result.errors)) {
+      return res.json(result.errors);
+    }
+    if (req.body.email && req.body.password) {
+      let email = req.body.email.trim();
+      console.log("BODY IS ", req.body);
+      console.log("Email IS ", email);
+      User.findOne({ email: email }, (err, user) => {
+        // console.log(user)
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ status: 500, data: null, errors: true, message: "Error while finding the user" });
+        }
+        if (user) {
+          // console.log("USER FOUND")
+          bcrypt.compare(req.body.password, user.password, function (er, isMatched) {
+            if (er) {
+              console.log(er);
+              return res.status(401).json({ message: "Error in validating Credentials" });
+            }
+            if (isMatched) {
+              const u = user.toObject();
+              delete u.password;
+              req.user = u;
+              jwt.sign({ _id: u._id }, process.env.JWT_SECRET, function (err, token) {
+                if (err) {
+                  console.log(err);
+                  res.json({ message: "Error while generating token" })
+                }
+                else {
+                  user = user.toObject();
+                  delete user.password;
+                  req.user = user;
+                  res.json({ message: "Login successfull", token, user })
+                }
+              });
+            } else {
+              return res.status(401).json({ message: "Invalid Credentials" });
+            }
+          });
+        } else {
+          res.json({ message: "User not exist" });
+        }
+
+      })
+      // let password = req.body.password;
+      // User.find({ email: email, })
+    }
   }
 });
-router.post('/register2', async(req, res) => {
+router.post('/register2', async (req, res) => {
   console.log("REGISTER2")
   if (req.headers.token) {
     if (typeof req.headers.token == "string" && req.headers.token.trim() !== "") {
@@ -219,7 +219,7 @@ router.post('/verifyotp', async (req, res) => {
   if (req.headers.token) {
     if (typeof req.headers.token == "string" && req.headers.token.trim() !== "") {
       try {
-       let payload = jwt.verify(req.headers.token, process.env.JWT_SECRET)
+        let payload = jwt.verify(req.headers.token, process.env.JWT_SECRET)
         User.findById(payload._id, "-password").populate("role").exec().then(doc => {
           if (doc) {
             let u = doc.toObject();
@@ -268,6 +268,10 @@ router.post('/verifyotp', async (req, res) => {
             // newUser.role = 
             newUser.mobile_number = result.data.mobile_number;
             newUser.role = process.env.CUSTOMER_ROLE;//"5d5157820250e60017e64d42";
+            newUser.full_name = "";
+            newUser.landmark = "";
+            newUser.street_address = "";
+            newUser.city = "";
             newUser.dob = Date.now();
             newUser.anniversary = Date.now();
             newUser.user_id = "USR" + moment().year() + moment().month() + moment().date() + moment().hour() + moment().minute() + moment().second() + moment().milliseconds() + Math.floor(Math.random() * (99 - 10) + 10);
@@ -293,43 +297,43 @@ router.post('/verifyotp', async (req, res) => {
     })
   }
 });
-router.get('/me', async(req,res)=>{
+router.get('/me', async (req, res) => {
   if (req.headers.token) {
-      if (typeof req.headers.token == "string" && req.headers.token.trim() !== "") {
-          // console.log("HAS TOKEN");
-          // if (req.method === "GET") {
-          jwt.verify(req.headers.token, process.env.JWT_SECRET, (err, payload) => {
-              if (err) {
-                  console.log(err);
-                  res.json({ message: "Invalid token" })
-              } else {
-                  // req.user = payload;
-                  User.findById(payload._id, (e, d) => {
-                      if (e) {
-                          res.json({ message: "Error while retriving user details" })
-                      }
-                      if (d) {
-                          d = d.toObject();
-                          delete d.password;
-                          req.user = d;
-                          res.json({ user: d })
-                      }
-                      else {
-                          res.json({ message: "Your token is not valid anymore" });
-                      }
-                  })
-                  // res.json({ user: payload });
-              }
+    if (typeof req.headers.token == "string" && req.headers.token.trim() !== "") {
+      // console.log("HAS TOKEN");
+      // if (req.method === "GET") {
+      jwt.verify(req.headers.token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) {
+          console.log(err);
+          res.json({ message: "Invalid token" })
+        } else {
+          // req.user = payload;
+          User.findById(payload._id, (e, d) => {
+            if (e) {
+              res.json({ message: "Error while retriving user details" })
+            }
+            if (d) {
+              d = d.toObject();
+              delete d.password;
+              req.user = d;
+              res.json({ user: d })
+            }
+            else {
+              res.json({ message: "Your token is not valid anymore" });
+            }
           })
-          // } else {
-          //     res.json({ message: "INVALID ROUTE" });
-          // }
-      } else {
-          res.json({ message: "invalid token" })
-      }
+          // res.json({ user: payload });
+        }
+      })
+      // } else {
+      //     res.json({ message: "INVALID ROUTE" });
+      // }
+    } else {
+      res.json({ message: "invalid token" })
+    }
   } else {
-      console.log("THIS CALLED");
-      res.status(401).json({ message: "Unauthorized" })
+    console.log("THIS CALLED");
+    res.status(401).json({ message: "Unauthorized" })
   }
 });
 
