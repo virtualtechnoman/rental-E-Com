f4bacf60ab5dd63b5218f942dc04652f6d37e457
bhumanyukumar <bhumanyukumar@gmail.com>
2019-08-17 13:13:22 +0530

    user update controller modified

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,56 +5,58 @@ const Joi = require('joi');
 const userSchema = Joi.object({
   full_name: Joi.string().required(),
   password: Joi.string().required(),
-  email:Joi.string().email().required(),
-  role:Joi.string().required(),
-  mobile_number:Joi.string().required(),
-  is_active:Joi.boolean().required()
+  email: Joi.string().email().required(),
+  role: Joi.string().required(),
+  mobile_number: Joi.string().required(),
+  is_active: Joi.boolean().required()
   // repeatPassword: Joi.string().required().valid(Joi.ref('password')),
 })
 const userUpdateSchema = Joi.object({
-  full_name: Joi.string().required(),
-  password: Joi.string().required(),
-  email:Joi.string().email().required(),
-  role:Joi.string().required()
+  full_name: Joi.string().optional(),
+  password: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  mobile_number: Joi.string().optional(),
+  is_active: Joi.boolean().optional(),
+  role: Joi.string().optional()
 })
 const userRegisterSchema = Joi.object({
   full_name: Joi.string().required(),
   password: Joi.string().required(),
-  email:Joi.string().email().required(),
+  email: Joi.string().email().required(),
   // role:Joi.string().required()
 })
 const userMobileRegisterSchema = Joi.object({
   // full_name: Joi.string().required(),
   // password: Joi.string().required(),
-  mobile_number:Joi.string().required(),
+  mobile_number: Joi.string().required(),
   // role:Joi.string().required()
 })
 const userMobileLoginSchema = Joi.object({
   // full_name: Joi.string().required(),
   // password: Joi.string().required(),
-  mobile_number:Joi.string().required(),
+  mobile_number: Joi.string().required(),
   // role:Joi.string().required()
 })
 const userMobileOtpSchema = Joi.object({
   // full_name: Joi.string().required(),
   // password: Joi.string().required(),
-  mobile_number:Joi.string().required(),
-  otp:Joi.string().required()
+  mobile_number: Joi.string().required(),
+  otp: Joi.string().required()
   // role:Joi.string().required()
 })
 
 const userLoginSchema = Joi.object({
   password: Joi.string().required(),
-  email:Joi.string().email().required()
+  email: Joi.string().email().required()
 })
 module.exports = {
-  verifyCreate:verifyCreate,
-  verifyUpdate:verifyUpdate,
-  verifyRegister:verifyRegister,
-  verifyLogin:verifyLogin,
-  verifyMobileRegister:verifyMobileRegister,
-  verifyMobileLogin:verifyMobileLogin,
-  verifyMobileOtp:verifyMobileOtp
+  verifyCreate: verifyCreate,
+  verifyUpdate: verifyUpdate,
+  verifyRegister: verifyRegister,
+  verifyLogin: verifyLogin,
+  verifyMobileRegister: verifyMobileRegister,
+  verifyMobileLogin: verifyMobileLogin,
+  verifyMobileOtp: verifyMobileOtp
 }
 
 function verifyCreate(user) { return helper.validator(user, userSchema) }
