689e406759bb9be7cdc0104fd31a6e36dad1a8b6
sushant-gupta7 <sushantg.248@gmail.com>
2019-09-02 10:49:40 +0530

    deploy user

--- src/app/core/user/user.component.ts
+++ src/app/core/user/user.component.ts
@@ -239,7 +239,7 @@ export class UserComponent implements OnInit {
     this.submitted = false;
     this.editing = false;
     this.registerForm.reset();
-    this.initForm();
+    this.initForm();  
   }
 
   getUserRoles() {
